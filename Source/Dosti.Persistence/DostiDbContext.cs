18254aa42da50bf17f1f41b2417ef11578a0bbf9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-18 00:54:18 +0530

    Added the DbContext

---
+++ Source/Dosti.Persistence/DostiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Dosti.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dosti.Persistence
+{
+
+    public class DostiDbContext : DbContext
+    {
+        public DbSet<DostiUser> Users => Set<DostiUser>();
+
+        public DostiDbContext(DbContextOptions<DostiDbContext> options) : base(options)
+        {
+        }
+    }
+
+}
