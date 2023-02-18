6820e04545f32de8ff1ef1d17809472075e8bea7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-18 16:18:54 +0530

    Get Users

---
+++ Source/Dosti.Api/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Dosti.Data.Entities;
+using Dosti.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dosti.Api.Controllers;
+
+public class UsersController : BaseApiController
+{
+    private readonly DostiDbContext _dostiDbContext;
+
+    public UsersController(DostiDbContext dostiDbContext)
+    {
+        _dostiDbContext = dostiDbContext ?? throw new ArgumentNullException(nameof(dostiDbContext));
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<IReadOnlyCollection<DostiUser>>> GetUsers()
+    {
+        return await _dostiDbContext.Users.ToListAsync();
+    }
+
+}
