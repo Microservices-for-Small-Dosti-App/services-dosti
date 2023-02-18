4d6d5d4c5d08cd67fbc15e4ce19d8059f7111dc9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-18 16:01:38 +0530

    SQL Scripts

---
+++ SQLScripts/dbo.Users.data.sql
@@ -0,0 +1,19 @@
+﻿INSERT INTO [dbo].[Users] 
+	([Id], [UserName], [KnownAs], [CreatedDate], [CreatedBy], [ModifiedDate], [ModifiedBy]) 
+VALUES 
+	(NEWID(), 'Sri Varu', 'SriVaru', getdate(), 'Admin', getdate(), 'Admin')
+
+INSERT INTO [dbo].[Users] 
+	([Id], [UserName], [KnownAs], [CreatedDate], [CreatedBy], [ModifiedDate], [ModifiedBy]) 
+VALUES 
+	(NEWID(), 'Bob', 'Bob', getdate(), 'Admin', getdate(), 'Admin')
+
+INSERT INTO [dbo].[Users] 
+	([Id], [UserName], [KnownAs], [CreatedDate], [CreatedBy], [ModifiedDate], [ModifiedBy]) 
+VALUES 
+	(NEWID(), 'Jane', 'Jane', getdate(), 'Admin', getdate(), 'Admin')
+
+INSERT INTO [dbo].[Users] 
+	([Id], [UserName], [KnownAs], [CreatedDate], [CreatedBy], [ModifiedDate], [ModifiedBy]) 
+VALUES 
+	(NEWID(), 'Tom', 'Tom', getdate(), 'Admin', getdate(), 'Admin')
